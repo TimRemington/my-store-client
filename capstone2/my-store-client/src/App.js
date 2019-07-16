41b8f144e9eb3e4f2ad925eaa30a19b63ae7b205
Tim Remington <Tim.Remington29@gmail.com>
2019-07-16 15:45:17 -0600

    added authentication

--- capstone2/my-store-client/src/App.js
+++ capstone2/my-store-client/src/App.js
@@ -9,6 +9,7 @@ import Products from './components/ProductsList'
 import ProductDetail from './components/ProductDetail'
 import PaymentSuccess from './components/PaymentSuccess'
 import PaymentCancel from './components/PaymentCancel'
+import Profile from './components/Profile'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/products/:id' component={ProductDetail} />
         <Route path='/success' component={PaymentSuccess} />
         <Route path='/cancel' component={PaymentCancel} />
+        <Route path='/profile' component={Profile} />
       </Switch>
     </Router>
   );
