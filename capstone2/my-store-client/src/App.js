a67b941b70500eb742f3465f070f03e9bc834bcd
Tim Remington <Tim.Remington29@gmail.com>
2019-07-16 01:22:54 -0600

    added payment cancel page

--- capstone2/my-store-client/src/App.js
+++ capstone2/my-store-client/src/App.js
@@ -8,6 +8,7 @@ import Navigation from './components/Navigation'
 import Products from './components/ProductsList'
 import ProductDetail from './components/ProductDetail'
 import PaymentSuccess from './components/PaymentSuccess'
+import PaymentCancel from './components/PaymentCancel'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path='/products' component={Products} />
         <Route path='/products/:id' component={ProductDetail} />
         <Route path='/success' component={PaymentSuccess} />
+        <Route path='/cancel' component={PaymentCancel} />
       </Switch>
     </Router>
   );
---
+++ capstone2/my-store-client/src/components/PaymentCancel/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentCancel = () => {
+  return (
+    <h2>Your Payment Has Been Cancelled!</h2>
+  )
+}
+
+export default PaymentCancel
